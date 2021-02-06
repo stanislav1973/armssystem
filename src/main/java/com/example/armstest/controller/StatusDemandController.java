274b99caa3db3c437971d104bd49df50313ec5bc
stas <sgovorov569@gmail.com>
2021-02-07 04:48:54 +0500

    add edit,delete in table status demand controller.

--- src/main/java/com/example/armstest/controller/StatusDemandController.java
+++ src/main/java/com/example/armstest/controller/StatusDemandController.java
@@ -7,10 +7,9 @@ import com.example.armstest.data.StatusDemandRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 //@RequestMapping("/statusDemand")
@@ -21,18 +20,36 @@ public class StatusDemandController {
     public StatusDemandController(StatusDemandRepo statusDemandRepo) {
         this.statusDemandRepo = statusDemandRepo;
     }
+
     @ModelAttribute(name = "demand")
     public Demand demand(){
        return  new Demand();
     }
+
     @GetMapping("/statusDemand")
     public String statusDemandController(Model model){
-        model.addAttribute("statusDemand",statusDemandRepo.findAll());
+        model.addAttribute("statusDemandD",statusDemandRepo.findAll());
         return "statusDemand";
     }
-    @PostMapping("/statusDemand")
-    public String postStatusDemandController(@ModelAttribute Demand demand){
-        return "redirect:/tableDemand";
+    @GetMapping("/editStatusDemand/{id}")
+    public String statusDemandControllerId(@PathVariable("id") long id,Model model){
+        StatusDemand statusDemand = statusDemandRepo.findById(id).orElseThrow(() -> new IllegalArgumentException("Exception" + id));
+        model.addAttribute("statusD",statusDemand);
+        return "statusDemand";
     }
 
+    @PostMapping("/editStatusDemand/{id}")
+    public String postStatusDemandController(@PathVariable("id") long id, @Valid StatusDemand statusDemand){
+        statusDemandRepo.save(statusDemand);
+        return "redirect:/statusDemand";
+    }
+
+
+    @GetMapping("/deleteStatusDemand/{id}")
+    public String deleteDemand(@PathVariable("id") long id) {
+        StatusDemand statusDemand = statusDemandRepo.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        statusDemandRepo.delete(statusDemand);
+        return "redirect:/tableDemand";
+    }
 }
