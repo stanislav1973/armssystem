33d8607b6bdb4e38e30285600165b199b64b9df7
stas <sgovorov569@gmail.com>
2021-02-10 22:40:05 +0500

    add new file DeviceController

--- src/main/java/com/example/armstest/Device.java
+++ src/main/java/com/example/armstest/Device.java
@@ -1,4 +1,15 @@
 package com.example.armstest;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "device")
 public class Device {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String namedevice;
 }
