03d29441e22966256c5fdb2cd191d9262dd11017
stas <sgovorov569@gmail.com>
2021-02-10 22:47:19 +0500

    add interface device

--- src/main/java/com/example/armstest/controller/DeviceController.java
+++ src/main/java/com/example/armstest/controller/DeviceController.java
@@ -1,9 +1,17 @@
 package com.example.armstest.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/devicename")
 public class DeviceController {
+
+@GetMapping
+public String deviceNameController(Model model){
+    //model.addAttribute("deviceC",)
+    return "devicename";
+}
 }
---
+++ src/main/java/com/example/armstest/data/DeviceNameRepo.java
@@ -0,0 +1,4 @@
+package com.example.armstest.data;
+
+public interface DeviceNameRepo {
+}
