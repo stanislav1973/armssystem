9a5b95e5404f848bcbfd4bda5a30e9065f6e9030
stas <sgovorov569@gmail.com>
2021-02-10 22:43:07 +0500

    add new html devicename

--- src/main/java/com/example/armstest/controller/DeviceController.java
+++ src/main/java/com/example/armstest/controller/DeviceController.java
@@ -1,4 +1,9 @@
 package com.example.armstest.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/devicename")
 public class DeviceController {
 }
